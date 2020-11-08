d71651c4015d3d49b62a5e8a6108253085eee6a9
stephanie <stephguirand@aol.com>
2020-11-08 02:31:11 +0000

    changed settings debug to False

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '=&84=v(v$zwyvby%monya-1qx^rggwb)$bjc)@l6cw4u+qyt(v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.stephanieguirand.com']
 
